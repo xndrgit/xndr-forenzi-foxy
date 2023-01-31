7a8343a65f63ddb658b90a5849698bac9e476dfb
XanderWorld <xander.mymails@gmail.com>
2023-01-31 20:15:25 +0100

    update

---
+++ resources/js/components/MainComponents/QuantityProductsComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="decrementQuantity" id="minus-button">-</button>
+        <input
+            class="font-weight-bold"
+            v-model="quantity"
+            type="number"
+            readonly
+            id="quantity-input"
+        />
+        <button @click="incrementQuantity" id="plus-button">+</button>
+    </div>
+</template>
+
+<script></script>
+
+<style lang="scss" scoped>
+input {
+    text-align: center;
+    max-width: 45px;
+    height: fit-content;
+}
+
+input,
+button {
+    border: 1px solid lightgrey;
+    font-size: 0.7rem;
+}
+#minus-button,
+#plus-button {
+    height: fit-content;
+    background-color: white;
+
+    text-align: center;
+    font-weight: bold;
+    cursor: pointer;
+}
+#minus-button:hover,
+#plus-button:hover {
+    background-color: #fdbc48;
+}
+</style>
