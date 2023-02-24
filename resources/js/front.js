1f47a52185b28a35eed4f4c6825e86fbd9dd195a
pullrepox <webdvpmaster103@gmail.com>
2023-02-24 16:15:31 +0100

    Updated store stuff

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,9 +1,8 @@
 import Vue from "vue";
-import Vuex from "vuex";
 import App from "./views/App";
 import VueRouter from "vue-router";
+import store from './store';
 import router from "./router.js";
-
 
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
@@ -14,47 +13,9 @@ require('./app');
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
-Vue.use(Vuex);
 Vue.use(Vuetify);
 Vue.use(Vuelidate);
 // Vue.use(Notifications);
-
-const store = new Vuex.Store({
-  state: {
-    isAuth: false,
-    name: "REGISTRATI",
-    productCount: 0,
-    total: 0.00,
-    quantity: [],
-  },
-  mutations: {
-    updateUser(state, user) {
-      state.isAuth = user.isAuth;
-      if (user.isAuth) {
-        state.name = user.name;
-        window.localStorage.setItem('user', user.name);
-      }
-    },
-    updateCart(state, info) {
-      state.productCount = info.productCount;
-      state.total = info.total;
-    },
-    currentQuantity(state, info) {
-      state.quantity = info.quantity;
-    }
-  },
-  getters: {
-    getUser (state) {
-        return state.name;
-    },
-    checkAuth (state) {
-        return state.isAuth;
-    }
-  },
-  actions: {
-  },
-  modules: {},
-});
 
 window.axios.interceptors.response.use(function (response) {
     return response
---
+++ resources/js/store/state.js
@@ -0,0 +1,10 @@
+const state = {
+    isAuth: false,
+    name: "REGISTRATI",
+    productCount: 0,
+    total: 0.00,
+    quantity: [],
+    cartItems: []
+};
+
+export default state;
