3a5d95486f0c76f9ec3f25980b0aad9a3243a0a6
pullrepox <webdvpmaster103@gmail.com>
2023-03-06 19:18:53 +0100

    Updated send mail

--- config/mail.php
+++ config/mail.php
@@ -63,14 +63,7 @@ return [
         ],
     ],
 
-    'driver'     => env('MAIL_DRIVER', 'smtp'),
-    'host'       => env('MAIL_HOST', 'smtp.gmail.com'),
-    'port'       => env('MAIL_PORT', 587),
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-    'username'   => env('MAIL_USERNAME'),
-    'password'   => env('MAIL_PASSWORD'),
     'sendmail'   => '/usr/sbin/sendmail -bs',
-    'pretend'    => false,
 
     /*
     |--------------------------------------------------------------------------
